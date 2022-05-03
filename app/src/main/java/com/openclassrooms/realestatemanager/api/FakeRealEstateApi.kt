9aa1dd431fe8ab895532fd3cd9121289b6f04a2a
Yohan <eras.projet@gmail.com>
2022-05-04 01:30:33 +0200

    Added Rxjava with Observable

--- app/src/main/java/com/openclassrooms/realestatemanager/api/FakeRealEstateApi.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/api/FakeRealEstateApi.kt
@@ -1,10 +1,11 @@
 package com.openclassrooms.realestatemanager.api
 
 import com.openclassrooms.realestatemanager.model.Property
+import io.reactivex.rxjava3.core.Observable
 
 class FakeRealEstateApi : RealEstateApi{
 
-    override fun getAllProperties(): List<Property> {
-        return ModelGenerator().getAllProperties()
+    override fun getAllProperties(): Observable<List<Property>> {
+        return Observable.just(ModelGenerator().getAllProperties())
     }
 }
--- app/src/main/java/com/openclassrooms/realestatemanager/api/RealEstateApi.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/api/RealEstateApi.kt
@@ -1,7 +1,8 @@
 package com.openclassrooms.realestatemanager.api
 
 import com.openclassrooms.realestatemanager.model.Property
+import io.reactivex.rxjava3.core.Observable
 
 interface RealEstateApi {
-    fun getAllProperties(): List<Property>
+    fun getAllProperties(): Observable<List<Property>>
 }
