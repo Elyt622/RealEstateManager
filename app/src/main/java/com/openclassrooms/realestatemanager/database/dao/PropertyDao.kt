dd2c1e70d19182d0ca94e04ce1a9edc8d661e430
Yohan <eras.projet@gmail.com>
2022-05-04 01:12:57 +0200

    Changed type MutableList to List & added onConflict for insert query

--- app/src/main/java/com/openclassrooms/realestatemanager/database/dao/PropertyDao.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/dao/PropertyDao.kt
@@ -2,6 +2,7 @@ package com.openclassrooms.realestatemanager.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.openclassrooms.realestatemanager.model.Property
 import io.reactivex.rxjava3.core.Observable
@@ -9,11 +10,11 @@ import io.reactivex.rxjava3.core.Single
 
 @Dao
 interface PropertyDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertProperty(property: Property) : Single<Long>
 
     @Query("SELECT * FROM Property")
-    fun getAllProperty() : Observable<MutableList<Property>>
+    fun getAllProperty() : Observable<List<Property>>
 
     @Query("SELECT * FROM Property WHERE ref = ref")
     fun getPropertyWithRef() : Single<Property>
