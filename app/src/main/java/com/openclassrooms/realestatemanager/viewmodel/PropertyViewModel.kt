cd29fade25a6ff32d64709a488bb7fcf63a71121
Yohan <eras.projet@gmail.com>
2022-05-04 01:32:47 +0200

    Added function to get a property with a reference

--- app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/PropertyViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/PropertyViewModel.kt
@@ -1,14 +1,30 @@
 package com.openclassrooms.realestatemanager.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.openclassrooms.realestatemanager.app.App
 import com.openclassrooms.realestatemanager.di.DI
 import com.openclassrooms.realestatemanager.model.Property
+import io.reactivex.rxjava3.core.Observable
 
 class PropertyViewModel : ViewModel() {
 
     private val api = DI().getService()
 
-    fun getAllProperties(): List<Property> {
-        return api.getAllProperties()
+    private val propertyDao = App.database.propertyDao()
+
+    private fun getAllProperties(): Observable<List<Property>> = api.getAllProperties()
+
+    //private fun getAllProperties(): Observable<List<Property>> = propertyDao.getAllProperty()
+
+    fun getPropertyWithRef(ref: Int): Property? {
+        var property : Property? = null
+        getAllProperties().subscribe { results ->
+            for (result in results) {
+                if (result.ref == ref)
+                     property = result
+            }
+        }
+        return property
     }
+
 }
