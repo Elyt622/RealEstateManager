83fb1facce7a779b0bebb03104715cc7d27500b4
Yohan <eras.projet@gmail.com>
2022-05-09 17:58:46 +0200

    Remove useless function

--- app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/PropertyViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/PropertyViewModel.kt
@@ -8,23 +8,10 @@ import io.reactivex.rxjava3.core.Observable
 
 class PropertyViewModel : ViewModel() {
 
-    private val api = DI().getService()
-
     private val propertyDao = App.database.propertyDao()
 
-    private fun getAllProperties(): Observable<List<Property>> = api.getAllProperties()
-
-    //private fun getAllProperties(): Observable<List<Property>> = propertyDao.getAllProperty()
-
-    fun getPropertyWithRef(ref: Int): Property? {
-        var property : Property? = null
-        getAllProperties().subscribe { results ->
-            for (result in results) {
-                if (result.ref == ref)
-                     property = result
-            }
-        }
-        return property
+    fun getPropertyWithRef(ref: Int): Observable<Property>{
+        return propertyDao.getPropertyWithRef(ref)
     }
 
 }
