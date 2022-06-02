a7b1306193089e717f204b8f0f4ff4cba1283830
Yohan <eras.projet@gmail.com>
2022-06-02 23:10:40 +0200

    Changed some element for design

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/adapter/TypeRvAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/adapter/TypeRvAdapter.kt
@@ -14,44 +14,46 @@ import com.openclassrooms.realestatemanager.databinding.CardviewRecyclerViewBind
 import com.openclassrooms.realestatemanager.model.Type
 import com.openclassrooms.realestatemanager.viewmodel.AddPropertyViewModel
 
-class TypeRvAdapter(private val viewModel: AddPropertyViewModel, private val context: Context, private val dataSet: Array<Type>) : RecyclerView.Adapter<TypeRvAdapter.ViewHolder>(){
+class TypeRvAdapter(private var viewModel: AddPropertyViewModel, private val context: Context, private val dataSet: Array<Type>)
+    : RecyclerView.Adapter<TypeRvAdapter.ViewHolder>(){
 
     private var clickedItem = -1
 
     private lateinit var binding: CardviewRecyclerViewBinding
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            binding = CardviewRecyclerViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            return ViewHolder(binding.root)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = CardviewRecyclerViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = dataSet[position].name
+        if (clickedItem != -1) {
+            if (clickedItem == position) {
+                holder.cardView.setCardBackgroundColor(ContextCompat.getColor(context, R.color.colorAccent))
+                holder.textView.setTextColor(Color.WHITE)
+            } else {
+                holder.cardView.setCardBackgroundColor(Color.WHITE)
+                holder.textView.setTextColor(ContextCompat.getColor(context, R.color.colorText))
+            }
         }
 
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.textView.text = dataSet[position].name
-            if (clickedItem != -1) {
-                if (clickedItem == position) {
-                    holder.cardView.setCardBackgroundColor(ContextCompat.getColor(context, R.color.colorPrimaryDark))
-                } else {
-                    holder.cardView.setCardBackgroundColor(Color.WHITE)
-                }
+        holder.cardView.setOnClickListener {
+            viewModel.getNewProperty().subscribe{ property ->
+                property.type = dataSet[position]
             }
-
-            holder.cardView.setOnClickListener {
-                viewModel.getNewProperty().subscribe{ property ->
-                    property.type = dataSet[position]
-                }
-
-                if (clickedItem != -1) {
-                    notifyItemChanged(clickedItem)
-                }
-                clickedItem = holder.adapterPosition
+            if (clickedItem != -1) {
                 notifyItemChanged(clickedItem)
             }
+            clickedItem = holder.adapterPosition
+            notifyItemChanged(clickedItem)
         }
+    }
 
-        override fun getItemCount() = dataSet.size
+    override fun getItemCount() = dataSet.size
 
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val textView: TextView = binding.textviewInterestPointRvPropertyActivity
-            val cardView: CardView = binding.cardviewOptionsRvOptions
-        }
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = binding.textviewInterestPointRvPropertyActivity
+        val cardView: CardView = binding.cardviewOptionsRvOptions
+    }
 }
