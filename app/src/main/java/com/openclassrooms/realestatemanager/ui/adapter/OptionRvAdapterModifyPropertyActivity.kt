12c44f9b6ba24c62c088d5135b3bcea0af1b4c5a
Yohan <eras.projet@gmail.com>
2022-06-02 23:14:49 +0200

    Added recyclerview adapter for options property in Modify Activity

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/adapter/OptionRvAdapterModifyPropertyActivity.kt
@@ -0,0 +1,78 @@
+package com.openclassrooms.realestatemanager.ui.adapter
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.databinding.CardviewRecyclerViewBinding
+import com.openclassrooms.realestatemanager.model.Option
+import com.openclassrooms.realestatemanager.viewmodel.ModifyPropertyViewModel
+import io.reactivex.rxjava3.kotlin.subscribeBy
+
+class OptionRvAdapterModifyPropertyActivity(
+    val viewModel: ModifyPropertyViewModel,
+    val context: Context,
+    private val dataset: Array<Option>,
+    listOptions: MutableList<Option>?
+    ) : RecyclerView.Adapter<OptionRvAdapterModifyPropertyActivity.ViewHolder>() {
+
+    private lateinit var binding : CardviewRecyclerViewBinding
+
+    private var clickedItems: BooleanArray = viewModel.getBooleanArrayWithListOptions(listOptions)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): OptionRvAdapterModifyPropertyActivity.ViewHolder {
+        binding = CardviewRecyclerViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(
+        holder: OptionRvAdapterModifyPropertyActivity.ViewHolder,
+        position: Int
+    ) {
+        holder.textView.text = dataset[position].displayName
+
+        if (clickedItems[position]) {
+            holder.cardView.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    context,
+                    R.color.colorAccent
+                )
+            )
+            holder.textView.setTextColor(Color.WHITE)
+        } else {
+            holder.cardView.setCardBackgroundColor(Color.WHITE)
+            holder.textView.setTextColor(ContextCompat.getColor(context, R.color.colorText))
+        }
+
+        holder.cardView.setOnClickListener {
+            clickedItems[position] = !clickedItems[position]
+            notifyItemChanged(position)
+            viewModel.getNewProperty().subscribeBy (
+                onNext = { results ->
+                    results.options = viewModel.getOptionsWithPositionInRV(clickedItems)
+                },
+                onComplete = {
+
+                },
+                onError = {
+
+                })
+        }
+    }
+
+    override fun getItemCount() = dataset.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = binding.textviewInterestPointRvPropertyActivity
+        val cardView: CardView = binding.cardviewOptionsRvOptions
+    }
+}
