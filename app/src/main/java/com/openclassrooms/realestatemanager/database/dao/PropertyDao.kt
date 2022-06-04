7e2f7f1e11831c197399d536ab46a2930dfb7c2d
Yohan <eras.projet@gmail.com>
2022-06-04 12:54:44 +0200

    Added update function

--- app/src/main/java/com/openclassrooms/realestatemanager/database/dao/PropertyDao.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/dao/PropertyDao.kt
@@ -1,12 +1,10 @@
 package com.openclassrooms.realestatemanager.database.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.openclassrooms.realestatemanager.model.Property
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 
 @Dao
 interface PropertyDao {
@@ -18,5 +16,8 @@ interface PropertyDao {
 
     @Query("SELECT * FROM Property WHERE ref = :ref")
     fun getPropertyWithRef(ref: Int) : Observable<Property>
+
+    @Update
+    fun updatePropertyWithRef(property: Property) : Single<Int>
 
 }
