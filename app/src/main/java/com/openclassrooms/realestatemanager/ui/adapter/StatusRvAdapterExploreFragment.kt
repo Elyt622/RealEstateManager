3ce470aa21a78ce491a7ddf8320b80d2c3fcb620
Yohan <eras.projet@gmail.com>
2022-06-07 18:41:45 +0200

    Added status adapter for recycler view in home fragment

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/adapter/StatusRvAdapterExploreFragment.kt
@@ -0,0 +1,62 @@
+package com.openclassrooms.realestatemanager.ui.adapter
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.databinding.CardviewRecyclerViewBinding
+import com.openclassrooms.realestatemanager.model.Status
+import com.openclassrooms.realestatemanager.viewmodel.ExploreViewModel
+
+class StatusRvAdapterExploreFragment (val viewModel: ExploreViewModel, val context: Context?, private val dataSet: Array<Status>)
+    : RecyclerView.Adapter<StatusRvAdapterExploreFragment.ViewHolder>() {
+
+    private lateinit var binding: CardviewRecyclerViewBinding
+
+    private var clickedItems = BooleanArray(dataSet.size) { false }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = CardviewRecyclerViewBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = dataSet[position].displayName
+
+        if (clickedItems[position]) {
+            holder.cardView.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    context!!,
+                    R.color.colorAccent
+                )
+            )
+            holder.textView.setTextColor(Color.WHITE)
+        } else {
+            holder.cardView.setCardBackgroundColor(Color.WHITE)
+            holder.textView.setTextColor(ContextCompat.getColor(context!!, R.color.colorText))
+        }
+
+        holder.cardView.setOnClickListener {
+            clickedItems[position] = !clickedItems[position]
+            notifyItemChanged(position)
+            viewModel.setStatus(viewModel.getStatusWithPositionInRV(clickedItems))
+        }
+    }
+
+    override fun getItemCount() = dataSet.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = binding.textviewInterestPointRvPropertyActivity
+        val cardView: CardView = binding.cardviewOptionsRvOptions
+    }
+}
