77b5f17a6b2a6ca9a2f643ee7a670b887dd423a0
Yohan <eras.projet@gmail.com>
2022-06-07 09:24:35 +0200

    Updated explore layout & added recycler views into the fragment

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/fragment/ExploreFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/fragment/ExploreFragment.kt
@@ -6,7 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.openclassrooms.realestatemanager.databinding.ExploreFragmentBinding
+import com.openclassrooms.realestatemanager.model.Option
+import com.openclassrooms.realestatemanager.model.Status
+import com.openclassrooms.realestatemanager.model.Type
+import com.openclassrooms.realestatemanager.ui.adapter.OptionRvAdapterExploreFragment
+import com.openclassrooms.realestatemanager.ui.adapter.StatusRvAdapterExploreFragment
+import com.openclassrooms.realestatemanager.ui.adapter.TypeRvAdapterExploreFragment
 import com.openclassrooms.realestatemanager.viewmodel.ExploreViewModel
 
 class ExploreFragment : Fragment() {
@@ -30,7 +37,55 @@ class ExploreFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this)[ExploreViewModel::class.java]
+        configTypeRv()
+        configOptionRv()
+        configStatusRv()
         // TODO: Use the ViewModel
+    }
+
+    private fun configTypeRv(){
+        with(binding){
+            recyclerViewType.layoutManager = LinearLayoutManager(
+                context,
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
+            recyclerViewType.adapter = TypeRvAdapterExploreFragment(
+                viewModel,
+                context,
+                Type.values()
+            )
+        }
+    }
+
+    private fun configOptionRv(){
+        with(binding){
+            recyclerViewOptions.layoutManager = LinearLayoutManager(
+                context,
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
+            recyclerViewOptions.adapter = OptionRvAdapterExploreFragment(
+                viewModel,
+                context,
+                Option.values()
+            )
+        }
+    }
+
+    private fun configStatusRv(){
+        with(binding){
+            recyclerViewStatus.layoutManager = LinearLayoutManager(
+                context,
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
+            recyclerViewStatus.adapter = StatusRvAdapterExploreFragment(
+                viewModel,
+                context,
+                Status.values()
+            )
+        }
     }
 
 }
