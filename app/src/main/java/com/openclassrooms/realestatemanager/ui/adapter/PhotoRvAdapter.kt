fbaa8957c8ef4b5030b318c6d1e538de03fc3916
Yohan <eras.projet@gmail.com>
2022-07-10 17:24:43 +0200

    Updated photos for the properties with the webp format into prepopulate database

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/adapter/PhotoRvAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/adapter/PhotoRvAdapter.kt
@@ -1,6 +1,7 @@
 package com.openclassrooms.realestatemanager.ui.adapter
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,12 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.integration.webp.decoder.WebpDrawable
+import com.bumptech.glide.integration.webp.decoder.WebpDrawableTransformation
+import com.bumptech.glide.load.Transformation
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.openclassrooms.realestatemanager.databinding.PhotosPropertyRecyclerViewBinding
+
 
 class PhotoRvAdapter(
     private val descriptionPhoto: MutableList<String>,
@@ -28,7 +34,12 @@ class PhotoRvAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Glide.with(context).load(dataSet[position].toString()).into(holder.image)
+        val circleCrop: Transformation<Bitmap> = CircleCrop()
+        Glide
+            .with(context)
+            .load(dataSet[position].toString())
+            .optionalTransform(WebpDrawable::class.java, WebpDrawableTransformation(circleCrop))
+            .into(holder.image)
         holder.descriptionImage.text = descriptionPhoto[position]
         holder.image.setOnClickListener {
             Glide.with(context).load(dataSet[position].toString()).into(mainImage)
